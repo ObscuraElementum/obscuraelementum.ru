5ce9b4e2439e9197a35785d37295532500b2e790
GWarp <gwarp56@gmail.com>
2015-11-02 14:09:06 +0300

    download add =)

--- bilders/showtopic.php
+++ bilders/showtopic.php
@@ -47,7 +47,10 @@ if (isset($_SESSION[nik])) {
 }
 		print('<div class="content" id="cb">
  <div class="contenttext">
- <header><time class="timekom" id="time" datetime="'.$result[date].'">'.$result[date].'</time> <h3 id="title">'.$result[name]. '</h3><h4 id="avtor">'.$r[nik].'</h4></header>'.$result[img].' <p id="prev">'.$result[info].'</p><footer></footer></div>');
+ <header><time class="timekom" id="time" datetime="'.$result[date].'">'.$result[date].'</time> <h3 id="title">'.$result[name]. '</h3><h4 id="avtor">'.$r[nik].'</h4></header>'.$result[img].' <p id="prev">'.$result[info].'</p><footer>');
+if($result[download]==NULL){
+	print'</footer></div>';
+}else{print "<a href='$result[download]'>Скачать</a></footer>";}
 print '  <div id="comments" name="comm">';
 for ($i=0; 	$i<$num;	$i++) {
 	$namee=$db->prepare("SELECT * FROM npc WHERE id=".$com[$col][id]);
