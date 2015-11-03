563d4f71b3d4f9d6a6093ef08a2bb0da349efba7
GWarp <gwarp56@gmail.com>
2015-11-03 09:21:19 +0300

    теперь костыли пашут, но остался косяк...

--- newtopic.php
+++ newtopic.php
@@ -36,8 +36,8 @@ $stg->execute();
 if($_FILES['userfile']['type']==' image/gif'or $_FILES['userfile']['type']=='image/png'or $_FILES['userfile']['type']==' image/jpeg' or $_FILES['userfile']['type']==JPEG){
 	$go=true;
 }else{$go=false;}
-if($filtera==='WoT PC' && $filterb==='Моды'){$fil=2;}elseif($filtera==='WoT PC' && $filterb==='Мaнуал'){$fil=4;}elseif($filtera==='WoT PC' && $filterb==='Видео'){$fil=3;}
-elseif($filtera==='WoT Bitz' && $filterb==='Моды'){$fil=5;}elseif($filtera==='WoT Bitz' && $filterb==='Мaнуал'){$fil=7;}elseif($filtera==='WoT Bitz' && $filterb==='Видео'){$fil=6;}else{$fil=0;}
+if($filtera=='WoT PC' && $filterb=='Моды'){$fil=2;}elseif($filtera=='WoT PC' && $filterb=='Мaнуал'){$fil=4;}elseif($filtera=='WoT PC' && $filterb=='Видео'){$fil=3;}
+elseif($filtera=='WoT Bitz' && $filterb=='Моды'){$fil=5;}elseif($filtera=='WoT Bitz' && $filterb=='Мaнуал'){$fil=7;}elseif($filtera=='WoT Bitz' && $filterb=='Видео'){$fil=6;}else{$fil=0;}
 if ($_SESSION[auth]===100){
 	print join('', file('html/makeform.html'));
 }else{print'<p id=error>Вы не авторизованны!</p>';}
@@ -47,8 +47,8 @@ if (!empty($_POST[name])&&!empty($_POST[info])&&!empty($_POST[smalinfo])&& $_SES
 	$h=tempnam('media/img/',$_SESSION[npc]);
 	$s='/var/www/u0100975/data/www/obscuraelementum.ru/media/img/';
 	$h=str_replace($s, "", $h);
-	$h='<img src="/media/img/'.$h.'">';
-	copy($_FILES[userfile][tmp_name], 'media/img/');
+	copy($_FILES[userfile][tmp_name], 'media/img/'.$h.'.png');
+	$h='<img src="/media/img/'.$h.'.png">';
 	$str=array($_POST[name], $_POST[smalinfo], $_POST[info], $_SESSION[npc], $h, $fil, $_POST[dw]);
 	$sql='INSERT INTO topics (name, smallinfo, info, posted, img, filter, download) VALUES  (?, ?, ?, ?, ?, ?, ?)';
 	$sth=$db->prepare($sql);
